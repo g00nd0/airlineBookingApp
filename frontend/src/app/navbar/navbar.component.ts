9270241f2a978515037fb472d22db87093498d07
g00nd0 <mitchellgoon@hotmail.com>
2021-06-29 16:31:17 +0800

    Update navbar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../services/session.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  currentUser!: string;
+  loggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private sessionService: SessionService) {}
 
   ngOnInit(): void {
+    this.currentUser = this.sessionService.sessionGet();
   }
-
 }
