c1224f909f06d5e1fae4928c34bd83498d277221
g00nd0 <mitchellgoon@hotmail.com>
2021-06-30 13:53:11 +0800

    Fix notifications for create-bookings

--- frontend/src/app/create-bookings/create-bookings.component.ts
+++ frontend/src/app/create-bookings/create-bookings.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { Bookings } from '../models/bookings';
 import { Flights } from '../models/flights';
 import { BookingsService } from '../services/bookings.service';
@@ -8,12 +11,16 @@ import { BookingsService } from '../services/bookings.service';
   styleUrls: ['./create-bookings.component.css'],
 })
 export class CreateBookingsComponent implements OnInit {
-  // @Input() user!: User;
   flights!: Flights;
   bookings!: Bookings;
   allFlights!: Flights[];
   seatsToReserve!: number;
   selected!: number;
+
+  private _success = new Subject<string>();
+  failMessage = '';
+
+  @ViewChild('selfClosingAlert', { static: false }) selfClosingAlert!: NgbAlert;
 
   constructor(private bookingsService: BookingsService) {}
 
@@ -21,31 +28,58 @@ export class CreateBookingsComponent implements OnInit {
     this.bookingsService.getAllFlights().subscribe((allFlights) => {
       this.allFlights = allFlights;
     });
+    this._success.subscribe((message) => (this.failMessage = message));
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if (this.selfClosingAlert) {
+        this.selfClosingAlert.close();
+      }
+    });
+  }
+
+  createFailMessage(message: string) {
+    this._success.next(message);
   }
 
   onSubmitBooking() {
     this.bookingsService.getOneFlight(this.selected).subscribe((flights) => {
       this.flights = flights[0];
-      this.flights.seatsAvailable -= this.seatsToReserve;
-      this.bookingsService
-        .addBookingsToFlight(this.flights)
-        .subscribe((flights) => {
-          const { id, seatsAvailable, seatsTotal, ...rest } = this.flights;
 
-          const oneBookingEntry: Bookings = {
-            ...rest,
-            bookingAgent: sessionStorage.getItem('currentUser') || 'unknown',
-            status: 'Reserved',
-          };
+      console.log(this.flights);
 
-          for (let i = 0; i < this.seatsToReserve; i++) {
-            this.bookingsService
-              .addBookingEntry(oneBookingEntry)
-              .subscribe(() => {
-                console.log('booking success for ' + i);
-              });
-          }
-        });
+      if (this.flights) {
+        if (this.seatsToReserve <= this.flights.seatsAvailable) {
+          this.flights.seatsAvailable -= this.seatsToReserve;
+          this.bookingsService
+            .addBookingsToFlight(this.flights)
+            .subscribe((flights) => {
+              const { id, seatsAvailable, seatsTotal, ...rest } = this.flights;
+
+              const oneBookingEntry: Bookings = {
+                ...rest,
+                bookingAgent:
+                  sessionStorage.getItem('currentUser') || 'unknown',
+                status: 'Reserved',
+              };
+
+              for (let i = 0; i < this.seatsToReserve; i++) {
+                this.bookingsService
+                  .addBookingEntry(oneBookingEntry)
+                  .subscribe(() => {
+                    console.log('booking success for ' + i);
+                  });
+              }
+            });
+        } else if (this.seatsToReserve > this.flights.seatsAvailable) {
+          console.log(this.seatsToReserve);
+          this.createFailMessage(
+            "You've selected too many seats, please enter a number less than the available seats."
+          );
+        } else {
+          this.createFailMessage('Enter a valid number of seats.');
+        }
+      } else {
+        this.createFailMessage('Error, please check your inputs.');
+      }
     });
   }
 }
