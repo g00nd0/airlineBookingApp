8b14f88c9c67a226694c13e8a082c4bfab962b8d
g00nd0 <mitchellgoon@hotmail.com>
2021-07-05 04:10:45 +0800

    Update test suite config

--- tests/conf.js
+++ tests/conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   framework: "jasmine",
   seleniumAddress: "http://localhost:4444/wd/hub",
-  // specs: ["login.js", "register.js", "bookings.js","newUserFlow,js"],
+  specs: ["login.js", "register.js", "bookings.js", "newUserFlow,js"],
   // specs: ["newUserFlow.js"],
   onPrepare: function () {
     browser.manage().window().setSize(1280, 1024);
