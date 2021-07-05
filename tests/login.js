8cfc0f003a7bc76a44e79f106d38b31f80794f02
g00nd0 <mitchellgoon@hotmail.com>
2021-07-05 17:36:40 +0800

    Amend test cases for login and register

--- tests/login.js
+++ tests/login.js
@@ -42,35 +42,6 @@ describe("Agent Airline Booking App", function () {
       });
   });
 
-  it("should fail login, with only username entered", function () {
-    browser.get("http://localhost:4200/login");
-
-    element(by.id("username")).sendKeys("g00nd0");
-
-    element(by.xpath('//*[@id="loginSubmit"]'))
-      .click()
-      .then(function () {
-        expect(element(by.tagName("ngb-alert")).getText()).toContain(
-          "Crendentials cannot be empty, please enter your username and password."
-        );
-      });
-  });
-
-  it("should fail login, when entering non-existent username", function () {
-    browser.get("http://localhost:4200/login");
-
-    element(by.id("username")).sendKeys("idontexist42069");
-    element(by.id("password")).sendKeys("thispasswordis$h!t");
-
-    element(by.xpath('//*[@id="loginSubmit"]'))
-      .click()
-      .then(function () {
-        expect(element(by.tagName("ngb-alert")).getText()).toContain(
-          "Login Error, user does not exist, please register for a new account."
-        );
-      });
-  });
-
   it("should fail login, when entering existing username with wrong password", function () {
     browser.get("http://localhost:4200/login");
 
@@ -135,23 +106,6 @@ describe("Agent Airline Booking App", function () {
       });
   });
 
-  // forget password fail - one field blank
-  it("should not reset password, for incomplete form with 1 empty field", function () {
-    browser.get("http://localhost:4200/pw-reset");
-    browser.waitForAngular();
-    element(by.name("username")).sendKeys("g00nd0");
-    element(by.name("password")).sendKeys("abcd1234");
-
-    element(by.xpath('//*[@id="resetSubmit"]'))
-      .click()
-      .then(function () {
-        browser.sleep(2000);
-        expect(element(by.tagName("ngb-alert")).getText()).toContain(
-          "Fields cannot be empty, make sure that the fields are filled in."
-        );
-      });
-  });
-
   // forget password fail - all fields blank
   it("should not reset password, for incomplete form with 1 empty field", function () {
     browser.get("http://localhost:4200/pw-reset");
@@ -163,77 +117,6 @@ describe("Agent Airline Booking App", function () {
         browser.sleep(2000);
         expect(element(by.tagName("ngb-alert")).getText()).toContain(
           "Fields cannot be empty, make sure that the fields are filled in."
-        );
-      });
-  });
-
-  // forget password fail - pw no match
-  it("should not reset password, for incomplete form with 1 empty field", function () {
-    browser.get("http://localhost:4200/pw-reset");
-    browser.waitForAngular();
-    element(by.name("username")).sendKeys("g00nd0");
-    element(by.name("password")).sendKeys("abcd1234");
-    element(by.name("passwordConfirm")).sendKeys("abcd12");
-
-    element(by.xpath('//*[@id="resetSubmit"]'))
-      .click()
-      .then(function () {
-        expect(element(by.tagName("ngb-alert")).getText()).toContain(
-          "Your passwords do not match."
-        );
-      });
-  });
-
-  // forget password fail - pw is less than 8 chars long
-  it("should not reset password, pw is not 8 characters long", function () {
-    browser.get("http://localhost:4200/pw-reset");
-    browser.waitForAngular();
-    element(by.name("username")).sendKeys("g00nd0");
-    element(by.name("password")).sendKeys("abcd12");
-    element(by.name("passwordConfirm")).sendKeys("abcd12");
-
-    element(by.xpath('//*[@id="resetSubmit"]'))
-      .click()
-      .then(function () {
-        browser.sleep(1000);
-        expect(element(by.tagName("ngb-alert")).getText()).toContain(
-          "Password must be alphanumeric, at least 8 characters long and contain no spaces."
-        );
-      });
-  });
-
-  // forget password fail - pw is not alphanumeric
-  it("should not reset password, pw is not alphanumeric", function () {
-    browser.get("http://localhost:4200/pw-reset");
-    browser.waitForAngular();
-    element(by.name("username")).sendKeys("g00nd0");
-    element(by.name("password")).sendKeys("abcdabcd ");
-    element(by.name("passwordConfirm")).sendKeys("abcdabcd");
-
-    element(by.xpath('//*[@id="resetSubmit"]'))
-      .click()
-      .then(function () {
-        browser.sleep(1000);
-        expect(element(by.tagName("ngb-alert")).getText()).toContain(
-          "Password must be alphanumeric, at least 8 characters long and contain no spaces."
-        );
-      });
-  });
-
-  // forget password fail - pw has space (' ')
-  it("should not reset password, pw contains white spaces", function () {
-    browser.get("http://localhost:4200/pw-reset");
-    browser.waitForAngular();
-    element(by.name("username")).sendKeys("g00nd0");
-    element(by.name("password")).sendKeys("abc d1234");
-    element(by.name("passwordConfirm")).sendKeys("abc d1234");
-
-    element(by.xpath('//*[@id="resetSubmit"]'))
-      .click()
-      .then(function () {
-        browser.sleep(1000);
-        expect(element(by.tagName("ngb-alert")).getText()).toContain(
-          "Password must be alphanumeric, at least 8 characters long and contain no spaces."
         );
       });
   });
--- tests/register.js
+++ tests/register.js
@@ -16,40 +16,6 @@ describe("Agent Airline Booking App", function () {
       });
   });
 
-  it("should pop up warning when entering password with less than 8 characters", function () {
-    element(by.id("password")).sendKeys("lousypw");
-    expect(element(by.xpath('//*[@id="regSubmit"]')).isEnabled()).toBe(false);
-    // expect(element(by.tagName("ngb-alert")).getText()).toContain(
-    //   "Password must be alphanumeric, at least 8 characters long and contain no spaces."
-    // );
-  });
-
-  it("should pop up warning when entering password that is not alphanumeric", function () {
-    element(by.id("password")).sendKeys("lousypeeword");
-    expect(element(by.xpath('//*[@id="regSubmit"]')).isEnabled()).toBe(false);
-    // expect(element(by.tagName("ngb-alert")).getText()).toContain(
-    //   "Password must be alphanumeric, at least 8 characters long and contain no spaces."
-    // );
-  });
-
-  it("should have no pop up warning, submit disabled, when entering alphanumeric password only", function () {
-    element(by.id("password")).sendKeys("lousypeeword123");
-    expect(element(by.xpath('//*[@id="regSubmit"]')).isEnabled()).toBe(false);
-  });
-
-  it("should pop up warning when entering invalid email format", function () {
-    element(by.id("email")).sendKeys("myemail@");
-    expect(element(by.tagName("ngb-alert")).getText()).toContain(
-      "Not a valid email address"
-    );
-    expect(element(by.xpath('//*[@id="regSubmit"]')).isEnabled()).toBe(false);
-  });
-
-  it("should have no pop up warning, submit disabled, when entering valid email format", function () {
-    element(by.id("email")).sendKeys("myemail@mail.com");
-    expect(element(by.xpath('//*[@id="regSubmit"]')).isEnabled()).toBe(false);
-  });
-
   it("should have submit disabled, when any field does not meet their respective requirement", function () {
     browser.sleep(1000);
     element(by.id("username")).sendKeys("mym");
